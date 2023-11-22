e142912bb6957164f99891409a39b34eec820ac1
andrey <avdeyka21@mail.ru>
2023-11-22 15:51:48 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print_hi = 1
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
